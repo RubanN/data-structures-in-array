11fefbdaaac993736841760e42125a2063810ac8
RubanN <vruban556@gmail.com>
2023-09-25 13:10:51 +0530

    data structure

---
+++ array.js
@@ -0,0 +1,10 @@
+let person ={
+      name:'ruban',
+      age:25,
+}
+let arr = ["apple","pineapple","cherry",person];
+arr.push("besant");
+// arr.pop()
+arr.unshift("ruban")
+arr.shift()
+console.log(arr);
